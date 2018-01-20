859cb620208d865f1577c2d4f4bc8dc77ef6692e
Divyam Pandya <divyampandya63@gmail.com>
2018-01-20 19:07:34 +0530

    updated

---
+++ app/src/main/java/com/example/divyampandya63/friendlychat/FriendlyMessage.java
@@ -0,0 +1,46 @@
+package com.example.divyampandya63.friendlychat;
+
+/**
+ * Created by divyampandya63 on 20/1/18.
+ */
+
+public class FriendlyMessage {
+
+    private String text;
+    private String name;
+    private String photoUrl;
+
+    public FriendlyMessage() {
+    }
+
+    public FriendlyMessage(String text, String name, String photoUrl) {
+        this.text = text;
+        this.name = name;
+        this.photoUrl = photoUrl;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+}
+
